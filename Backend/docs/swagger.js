2a13a05c3683c326a256f6547fe821dd668a1625
Anupam <anupam.r2301@gmail.com>
2026-06-21 13:12:10 +0530

    fixing swagger.js

--- Backend/docs/swagger.js
+++ Backend/docs/swagger.js
@@ -4,7 +4,7 @@ const options = {
   definition: {
     openapi: "3.0.0",
     info: {
-      title: "StackIt API",
+      title: "Question-Answer platform API",
       version: "1.0.0",
       description: `
       
