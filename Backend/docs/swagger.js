9e7afd539e189caf6262a0e81ea5de2284d67970
Anupam <anupam.r2301@gmail.com>
2026-06-24 00:15:26 +0530

    fixing bugs

--- Backend/docs/swagger.js
+++ Backend/docs/swagger.js
@@ -23,7 +23,7 @@ const options = {
 
     servers: [
       {
-        url: "http://localhost:1111",
+        url: "https://question-answer-platform-api.onrender.com/",
       },
     ],
     components: {
